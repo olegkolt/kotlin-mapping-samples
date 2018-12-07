7c14f174a2876174a0d7c2f1dce9e4ea03e897ac
olegkolt <olegkolt@mail.ru>
2018-12-07 18:39:48 +0300

    Add usage for class-mapper

--- ClassMapper.kt
+++ ClassMapper.kt
@@ -1,3 +1,5 @@
+// ==== data ====
+
 class PersonSrc(
     val name: String,
     val salary: SalarySrc
@@ -9,6 +11,8 @@ class PersonDst(
     val salary: SalaryDst
 )
 class SalaryDst(val amount: Int)
+
+// ==== mappers ====
 
 interface DataMapper<SOURCE, DESTINATION> {
     fun transform(data: SOURCE): DESTINATION
@@ -26,3 +30,11 @@ class PersonMapper(
 class SalaryMapper : DataMapper<SalarySrc, SalaryDst> {
     override fun transform(src: SalarySrc) = SalaryDst(src.amount)
 }
+
+// ==== usage ====
+
+val personSrc = PersonSrc("Oleg", SalarySrc(10))
+val personMapper = PersonMapper(SalaryMapper())
+
+
+val personDst = personMapper.transform(personSrc)
